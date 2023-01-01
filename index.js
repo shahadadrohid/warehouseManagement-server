7e6ff25a956a8e0cba7708a9c418296abff296bf
Mohammad Shahadad Hossain <shahadadrohid@gmail.com>
2023-01-01 13:39:59 +0600

    Inventory item per id delete

--- index.js
+++ index.js
@@ -59,6 +59,13 @@ async function run() {
             res.send(result)
             console.log(result)
         })
+
+        app.delete('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     catch {
 
