8f191f3e5d1c443c3cf688fb6de2f3f95246263a
Mohammad Shahadad Hossain <shahadadrohid@gmail.com>
2023-01-01 13:32:48 +0600

    Server connect with mongodb atlas

--- index.js
+++ index.js
@@ -2,10 +2,31 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xezokrr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('warehouseManagement').collection('inventory');
+    }
+    catch {
+
+    }
+}
+run().catch(console.dir)
+
+
 
 
 
