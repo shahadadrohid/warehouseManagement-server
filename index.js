9732ddef0e821272b7049025f30ac5ac37ae1b88
Mohammad Shahadad Hossain <shahadadrohid@gmail.com>
2023-01-01 13:44:17 +0600

    Object id update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
